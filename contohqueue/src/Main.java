0143aa0a43a254993204e6649d81a728763593e2
Muamarxandrya1 <115987370+Jonathajo1@users.noreply.github.com>
2022-12-17 23:39:38 +0700

    Add files via upload

---
+++ contohqueue/src/Main.java
@@ -0,0 +1,19 @@
+import antrian.Antrian;
+
+public class Main {
+
+
+        public static void main(String[] args){
+            Antrian antrian = new Antrian(10);
+            antrian.masuk(13);
+            antrian.lihat();
+            antrian.masuk(32);
+            antrian.lihat();
+            antrian.masuk(45);
+            antrian.lihat();
+            System.out.println("yang diambil dari antrian = " + antrian.keluar());
+            antrian.lihat();
+            System.out.println("yang diambil dari antrian = " + antrian.keluar());
+            antrian.lihat();
+        }
+    }
